7f514c695e6653564fee946452faaffd4635800a
ahmed <ahmedfraag255@gmail.com>
2025-12-02 02:31:04 +0200

    initilize app

---
+++ wetherMVI/wetherMVI/wetherMVIApp.swift
@@ -0,0 +1,17 @@
+//
+//  wetherMVIApp.swift
+//  wetherMVI
+//
+//  Created by . on 01/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct wetherMVIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
