06710a83e79eba11cf605fd76fa5cf4114179f5f
ahmed <ahmedfraag255@gmail.com>
2025-12-06 08:42:07 +0200

    add wether Store to achive Intent-store structure for the design pattern

---
+++ wetherMVI/wetherMVI/Store/WeatherStore.swift
@@ -0,0 +1,99 @@
+//
+//  WeatherStore.swift
+//  wetherMVI
+//
+//  Created by . on 06/12/2025.
+//
+
+import Foundation
+import Combine
+import SwiftUI
+
+class WeatherStore: ObservableObject {
+    struct State {
+        var currentweather: ForecastResponse?
+        var hoursPoints: [HourlyPoint]?
+        var dailyPoints: [DailyPoint]?
+        var isLoading: Bool = false
+        var error: Error?
+    }
+    
+    @Published private(set) var state: State = State()
+    
+    // intents
+    enum Intent {
+        case fetchcurrentweather
+        case fetchWetherinLocation(city: String)
+        
+        case startPolling(interval: TimeInterval)
+        case stopPolling
+    }
+    
+    // dependancies
+    private let api: ForecastApiServiceProtocol
+    private var cancellables: Set<AnyCancellable> = []
+    private var pollingCancellable: AnyCancellable?
+    
+    init(api: ForecastApiServiceProtocol) {
+        self.api = api
+        api.dailyPublisher
+            .receive(on: DispatchQueue.main)
+            .sink {[weak self] points in
+                Task { @MainActor in
+                    self?.state.dailyPoints = points
+                }
+            }.store(in: &cancellables)
+        
+        api.hourlyPublisher.receive(on: DispatchQueue.main).sink { [weak self] points in
+            Task { @MainActor in
+                self?.state.hoursPoints = points
+            }
+        }.store(in: &cancellables)
+    }
+    
+    func send(_ intent: Intent) {
+            switch intent {
+            case .fetchcurrentweather:
+                Task { await performRefresh() }
+            case .startPolling(let interval):
+                pollingCancellable = api.startPolling(every: interval)
+            case .stopPolling:
+                pollingCancellable?.cancel()
+                pollingCancellable = nil
+            
+            case .fetchWetherinLocation(let city):
+                Task{
+                    await performfetchWetherforLocation(city: city)
+                }
+            }
+        }
+    private func performRefresh() async {
+        state.isLoading = true
+        state.error = nil
+           do {
+               let resp = try await api.fetchForecast()
+               // optionally map current temp
+               state.currentweather = resp
+               
+               // hourly & daily are emitted via combine subjects and already assigned via sinks
+           } catch {
+               state.error = error
+           }
+           state.isLoading = false
+       }
+    
+    private func performfetchWetherforLocation(city: String) async {
+        state.isLoading = true
+        state.error = nil
+           do {
+               let resp = try await api.fetchForecast(for: city)
+               // optionally map current temp
+               state.currentweather = resp
+               
+               // hourly & daily are emitted via combine subjects and already assigned via sinks
+           } catch {
+               state.error = error
+           }
+           state.isLoading = false
+       }
+}
